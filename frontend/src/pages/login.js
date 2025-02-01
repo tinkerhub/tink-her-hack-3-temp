0e5f69110fd8f45b8f2e01eaed957f6c7c960ec8
rose <rosefrancisrose16@gmail.com>
2025-02-01 22:54:05 +0900

    loginWithCSS

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "../styles/login.css"; 
 
 const Login = () => {
   const [email, setEmail] = useState("");
