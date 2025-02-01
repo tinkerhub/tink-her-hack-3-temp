e08d5b5ea79a9703a5a50666483ef4bf8fd6d74e
esha <eshasusanshaji@gmail.com>
2025-02-01 22:01:53 +0530

    about

---
+++ frontend/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="page">
+      <h1>About Us</h1>
+      <p>We connect donors with organizations helping cancer patients.</p>
+    </div>
+  );
+};
+
+export default About;
