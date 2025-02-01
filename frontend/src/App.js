29610bde3e634c4fa00c6453d783357873405f6d
sreepriyakmo4 <147272656+sreepriyakmo4@users.noreply.github.com>
2025-02-01 21:41:05 +0530

    loginhome

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <AppRoutes />
     </Router>
   );
--- frontend/src/routes/Approutes.js
+++ frontend/src/routes/Approutes.js
@@ -2,18 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
-import About from "../pages/About";
-import OrganisationsList from "../pages/OrganisationsList";
-import RegisterDonor from "../pages/RegisterDonor";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/organisations" element={<OrganisationsList />} />
-      <Route path="/register-donor" element={<RegisterDonor />} />
     </Routes>
   );
 }
