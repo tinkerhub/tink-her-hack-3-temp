9f3344c7fdb8d3acea9ed48c5193b4d393aa8b60
Aneeja55 <aneeja5505@gmail.com>
2025-02-08 21:26:50 +0530

    Add middleware for authentication

--- Backend/Server.js
+++ Backend/Server.js
@@ -3,9 +3,15 @@ const express= require('express');
 const bodyParser= require('body-parser');
 const mongoose= require('mongoose');
 
+const authRoutes= require('./routes/auth');
+const scanRoutes= require('./routes/scan');
+
 const app=express();
 app.use(corse());
 app.use(bodyParser.json());
+
+app.use('/api/auth', authRoutes);
+app.use('/api/scan', scanRoutes);
 
 mongoose.connect('mongodb://localhost:27017/digital-mess-card', {  //Mongodb connection
     useNewUrlParser: true,
